2de3e103a9d0d1d7f62196ba88a71c06f59e53e9
Гарвель Локен <Staaalker@tut.by>
2018-12-21 23:47:51 +0300

    table added

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,48 @@
-/* eslint-disable */
 import React from 'react';
-import BootstrapTable from 'react-bootstrap-table-next';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
-import cellEditFactory from 'react-bootstrap-table2-editor';
 
-const columns = [{
-  dataField: 'col1',
-  text: 'Column 1',
-  
-}, {
-  dataField: 'col1',
-  text: 'Column 2',
-  
-}, {
-  dataField: 'col3',
-  text: 'Column 3',
- 
-}, {
-  dataField: 'col4',
-  text: 'Column 4',
-  
-}];
+export default class BichTable extends React.PureComponent {
+  getColor = (id) => {
+    let prevColor = document.getElementById(id).style.backgroundColor;
+    const color = prompt('Введите цвет', prevColor);
+    document.getElementById(id).style.backgroundColor = color ? color : prevColor;
+  }
 
-const products = [{
-  col1: 'rgba(71, 78, 165, 0.51)',
-  col2: 'rgba(71, 78, 165, 0.51)',
-  col3: 'rgba(71, 78, 165, 0.51)',
-  col4: 'rgba(71, 78, 165, 0.51)',
-}, {
-  col1: 'rgba(165, 71, 71, 0.51)',
-  col2: 'rgba(165, 71, 71, 0.51)',
-  col3: 'rgba(165, 71, 71, 0.51)',
-  col4: 'rgba(165, 71, 71, 0.51)',
-}, {
-  col1: 'rgba(71, 78, 165, 0.51)',
-  col2: 'rgba(71, 78, 165, 0.51)',
-  col3: 'rgba(71, 78, 165, 0.51)',
-  col4: 'rgba(71, 78, 165, 0.51)',
-}, {
-  col1: 'rgba(165, 71, 71, 0.51)',
-  col2: 'rgba(165, 71, 71, 0.51)',
-  col3: 'rgba(165, 71, 71, 0.51)',
-  col4: 'rgba(165, 71, 71, 0.51)',
-}];
-
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <BootstrapTable
-        bootstrap4
-        keyField="id"
-        data={products}
-        columns={columns}
-        hover
-        classes="foo"
-        cellEdit={ cellEditFactory({ mode: 'click' }) }
-      />
-      
-    </header>
-  </div>
-
-);
-
-export default App;
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <table className="bich-table">
+            <tr>
+              <th id="1" onClick={() => this.getColor('1')}>Firstname</th>
+              <th id="2" onClick={() => this.getColor('2')}>Lastname</th> 
+              <th id="3" onClick={() => this.getColor('3')}>Age</th>
+              <th id="4" onClick={() => this.getColor('4')}>Sex</th>
+            </tr>
+            <tr>
+              <td id="5" onClick={() => this.getColor('5')}>Jill</td>
+              <td id="6" onClick={() => this.getColor('6')}>Smith</td> 
+              <td id="7" onClick={() => this.getColor('7')}>50</td>
+              <td id="8" onClick={() => this.getColor('8')}>Men</td>
+            </tr>
+            <tr>
+              <td id="9"  onClick={() => this.getColor('9')}>Eve</td>
+              <td id="10" onClick={() => this.getColor('10')}>Jackson</td> 
+              <td id="11" onClick={() => this.getColor('11')}>94</td>
+              <td id="12" onClick={() => this.getColor('12')}>Men</td>
+            </tr>
+            <tr>
+              <td id="13" onClick={() => this.getColor('13')}>Artem</td>
+              <td id="14" onClick={() => this.getColor('14')}>Balamut</td> 
+              <td id="15" onClick={() => this.getColor('15')}>94</td>
+              <td id="16" onClick={() => this.getColor('16')}>Women</td>
+            </tr>
+          </table>
+        </header>
+      </div>
+    );
+  }
+}
